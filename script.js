8455d9f558f35482e1e719eb76e49a213e253078
hellodeveloper123 <mdsabiha2002@gmail.com>
2023-12-22 15:40:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let buttons=document.querySelector('.buttons');
+let btn=buttons.querySelectorAll('span');
+let value=document.getElementById('value');
+
+for(let i=0;i<btn.length;i++){
+   btn[i].addEventListener('click',function(){
+      if(this.innerHTML=="="){
+        value.innerHTML=eval(value.innerHTML);
+      }
+      else if(this.innerHTML=='Clear'){
+        value.innerHTML="";
+      }
+      else if (this.innerHTML == 'Del') {
+        value.innerHTML = value.innerHTML.slice(0, -1);
+      }
+      else{
+        value.innerHTML+=this.innerHTML;
+      }
+   })
+}
